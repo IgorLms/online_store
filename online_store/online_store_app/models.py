5dcb94949d09e7672b9267bc9f39bd79e1e03d3a
IgorLms <sigor97@mail.ru>
2023-11-07 16:43:05 +0300

    Изменил модель User

--- online_store/online_store_app/models.py
+++ online_store/online_store_app/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from rest_framework.authtoken.models import Token
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+from .managers import CustomUserManager
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
+
+
+class UserCustom(AbstractBaseUser, PermissionsMixin):
+    username = None
+    email = models.EmailField('email', max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
